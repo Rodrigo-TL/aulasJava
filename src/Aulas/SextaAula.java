8e41d6a1f6af254a89187950493ac25c7404d553
Rodrigo-TL <rodrigodqbrn@gmail.com>
2026-07-17 21:46:45 -0300

    atividade 6 - atualização

---
+++ src/Aulas/SextaAula.java
@@ -0,0 +1,20 @@
+package Aulas;
+
+import Aulas.objeto.pessoa;
+
+public class SextaAula {
+    public static void main(String[] args) {
+
+        System.out.println ();
+
+
+        }
+
+        public String nome;
+        public int anoNascimento;
+        public char sexualidade;
+        public String cor;
+
+
+    }
+}
---
+++ src/Aulas/objeto/pessoa.java
@@ -0,0 +1,6 @@
+package Aulas.objeto;
+
+public class pessoa {
+
+
+}
