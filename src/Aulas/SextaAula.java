0f282aeefc4fc72209e103d7a39ea30fcd0cb413
Rodrigo-TL <rodrigodqbrn@gmail.com>
2026-07-20 20:43:55 -0300

    Aula 6 - atualização

--- src/Aulas/SextaAula.java
+++ src/Aulas/SextaAula.java
@@ -4,17 +4,20 @@ import Aulas.objeto.pessoa;
 
 public class SextaAula {
     public static void main(String[] args) {
+        pessoa pessoa1 = new pessoa(
+                "João",
+                2004,
+                'M',
+                "Branco",
+                "Professor"
+        );
 
-        System.out.println ();
+        System.out.println(pessoa1.getNome());
 
+        System.out.println(pessoa1);
 
-        }
+        pessoa1.setNome("Fulano de tal");
 
-        public String nome;
-        public int anoNascimento;
-        public char sexualidade;
-        public String cor;
-
-
+        System.out.println(pessoa1);
     }
 }
